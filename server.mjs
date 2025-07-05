fbc773ed248405fa63395055649751c129c3f96b
MalikaLaouiti <laouiti.malika@yahoo.com>
2025-07-05 22:37:58 +0100

    adding nodemon to automatically restarts my node application

--- server.mjs
+++ server.mjs
@@ -1,20 +1,16 @@
-const express = require('express');
-const next = require('next');
+import next from 'next';
+import express from 'express';
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
+// const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   const server = express();
 
   server.get('/api/hello', (req, res) => {
-    res.json({ message: 'Hello from Express!' });
-  });
-
-  server.all('*', (req, res) => {
-    return handle(req, res);
+    res.json({ message: 'Hello from Express' });
   });
 
   server.listen(port, () => {
